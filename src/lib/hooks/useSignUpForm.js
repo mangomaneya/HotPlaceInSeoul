7a3dd39cec003eb9188163176011d991f2b26508
choichangyeon <changyon99@naver.com>
2025-02-27 20:59:06 +0900

    fix:#8 error 메시지 상수화

--- src/lib/hooks/useSignUpForm.js
+++ src/lib/hooks/useSignUpForm.js
@@ -3,6 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { signUpValidate } from '@utils/signUpValidate';
 import supabase from '@api/supabaseAPI';
 
+const errorMessageText = {
+  DUPLICATED: '중복 체크를 해주세요.',
+  INVALID_EMAIL: '사용 불가능한 이메일 입니다.',
+  VALID_EMAIL: '사용가능한 이메일 입니다.',
+  LIMIT_NICKNAME: '최소 2자부터 최대 8자까지 가능합니다.',
+  VALID_NICKNAME: '사용가능한 닉네임 입니다.',
+};
+
 export default function useSignUp() {
   const [signUpFormData, setSignUpFormData] = useState({
     email: '',
@@ -32,20 +40,20 @@ export default function useSignUp() {
 
   async function checkDuplicate(type) {
     if (type === 'email') {
-      if (errorMessage.email !== '중복 체크를 해주세요.') return;
+      if (errorMessage.email !== errorMessageText.DUPLICATED) return;
 
       const { data } = await supabase.from('users').select().eq('email', signUpFormData.email);
-      if (data.length !== 0) return setErrorMessage((prev) => ({ ...prev, [type]: '사용 불가능한 이메일 입니다.' }));
+      if (data.length !== 0) return setErrorMessage((prev) => ({ ...prev, [type]: errorMessageText.INVALID_EMAIL }));
 
       setIsDuplicateChecked((prev) => ({ ...prev, [type]: true }));
-      return setErrorMessage((prev) => ({ ...prev, [type]: '사용가능한 이메일 입니다.' }));
+      return setErrorMessage((prev) => ({ ...prev, [type]: errorMessageText.VALID_EMAIL }));
     }
 
     if (type === 'nickname') {
-      if (errorMessage.nickname === '최소 2자부터 최대 8자까지 가능합니다.') return;
+      if (errorMessage.nickname === errorMessageText.LIMIT_NICKNAME) return;
 
       setIsDuplicateChecked((prev) => ({ ...prev, [type]: true }));
-      return setErrorMessage((prev) => ({ ...prev, [type]: '사용가능한 닉네임 입니다.' }));
+      return setErrorMessage((prev) => ({ ...prev, [type]: errorMessageText.VALID_NICKNAME }));
     }
   }
 
