f4913776754bd7a388034c72371161950e6b09df
lee kiri <mango.man.e.ya@gmail.com>
2025-03-01 01:47:50 +0900

    test : 전역 상태 유저 정보 확인 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
+import useAuthStore from '@/store/zustand/authStore';
+
 export default function Home() {
-  return <div className='bg-point'>홈</div>;
+  //전역상태로 관리 될 유저정보 로직 확인을 위한 임시 작업 (삭제예정)
+  const userData = useAuthStore((state) => state.userData);
+  const { token, userId, userEmail, userNickname } = userData;
+  return (
+    <div className='bg-point'>
+      홈<p>access_token : {token}</p>
+      <p>id : {userId}</p>
+      <p>email : {userEmail}</p>
+      <p>nickname : {userNickname}</p>
+    </div>
+  );
 }
