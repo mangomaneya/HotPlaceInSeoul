546e92740c18db0b8fd567c868af5257541d4426
choichangyeon <changyon99@naver.com>
2025-02-28 16:52:20 +0900

    refactor:#8 유효성 검증 부분 수정

--- src/lib/utils/signUpValidate.js
+++ src/lib/utils/signUpValidate.js
@@ -6,13 +6,13 @@ export function signUpValidate(name, value, form) {
       return emailRegex.test(value) ? '중복 체크를 해주세요.' : '올바른 이메일 형식을 입력해주세요.';
     }
     case 'password': {
-      return passwordRegex.test(value) ? '' : '영문, 숫자, 특수기호를 포함하여 최소 8자이상 작성해주세요';
+      return !passwordRegex.test(value) && '영문, 숫자, 특수기호를 포함하여 최소 8자이상 작성해주세요';
     }
     case 'confirmPassword': {
-      return value === form.password ? '' : '입력하신 비밀번호와 일치하지 않습니다.';
+      return value !== form.password && '입력하신 비밀번호와 일치하지 않습니다.';
     }
     case 'nickname': {
-      return value.length > 8 || value.length <= 1 ? '최소 2자부터 최대 8자까지 가능합니다.' : '';
+      return (value.length > 8 || value.length <= 1) && '최소 2자부터 최대 8자까지 가능합니다.';
     }
     default:
       return '';
