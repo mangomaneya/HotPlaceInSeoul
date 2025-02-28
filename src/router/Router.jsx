af150486c073d8c75638d338e6c31a6679e62671
JinChaeKim <rlawlsco88@gmail.com>
2025-02-28 15:17:37 +0900

    feat : #14 북마크페이지 기본 UI

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
 import Layout from '@/components/layout/Layout';
 import { PATH } from '@/constants/path-constant';
-import BookMark from '@/pages/BookMark';
+import BookMark from '@/pages/BookMark/BookMark';
 const { LOGIN, SIGN_UP, BOOK_MARK } = PATH;
 export default function Router() {
   return (
@@ -14,6 +14,7 @@ export default function Router() {
           <Route path={LOGIN} element={<div>login</div>} />
           {/*  //TODO: 회원가입 페이지 연결해 주세요 */}
           <Route path={SIGN_UP} element={<div>회원가입</div>} />
+          {/* MOCK-DATA 제거 시 파일 위치 변경 필요 */}
           <Route path={BOOK_MARK} element={<BookMark />} />
         </Route>
       </Routes>
