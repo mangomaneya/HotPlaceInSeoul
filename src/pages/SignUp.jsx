4a63f38bd3598d5d8b6b3d43269f973fc88e722e
choichangyeon <changyon99@naver.com>
2025-02-28 15:14:56 +0900

    feat:#8 회원가입 페이지 완료

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,6 @@ import SignUpForm from '@/components/sign-up/SignUpForm';
 export default function SignUp() {
   return (
     <div className='flex flex-col justify-center items-center'>
-      Signup
       <SignUpForm />
     </div>
   );
