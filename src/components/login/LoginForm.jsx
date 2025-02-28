77a271851282a7ab79d246f498d6f060fb64588a
choichangyeon <changyon99@naver.com>
2025-02-28 21:10:01 +0900

    design:#21 input border 적용

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -8,10 +8,10 @@ const LoginForm = () => {
       onSubmit={loginSubmitHandler}
       className='flex flex-col h-[350px] w-[400px] py-[30px] px-[30px] border-[1px] shadow-md rounded-md justify-between'
     >
-      <div className='flex flex-col gap-[25px]'>
+      <div className='flex flex-col gap-[20px]'>
         <div className='mb-[10px]'>
           <InputForm
-            className='px-[5px] py-[5px] rounded-xl border border-button focus:border-accent focus:outline-none'
+            className='px-[5px] py-[5px] rounded-xl border border-input focus:border-accent focus:outline-none'
             labelName='이메일'
             name='email'
             type='text'
@@ -22,7 +22,7 @@ const LoginForm = () => {
         </div>
         <div className='mb-[10px]'>
           <InputForm
-            className='px-[5px] py-[5px] rounded-xl border border-point focus:border-accent focus:outline-none'
+            className='px-[5px] py-[5px] rounded-xl border border-input focus:border-accent focus:outline-none'
             labelName='비밀번호'
             name='password'
             type='password'
