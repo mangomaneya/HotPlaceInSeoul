6a990148fc45837c81825b99dbad2baf4130483c
lee kiri <mango.man.e.ya@gmail.com>
2025-03-02 00:09:41 +0900

    feat: ProtectedRouter 구조 반영 및 북마크 경로 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,16 +4,20 @@ import { PATH } from '@constants/path-constant';
 import Home from '@pages/Home';
 import SignUp from '@pages/SignUp';
 import Login from '@pages/Login';
-const { LOGIN, SIGN_UP, HOME } = PATH;
+import ProtectedRouter from './ProtectedRouter';
+const { LOGIN, SIGN_UP, HOME, BOOK_MARK } = PATH;
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={HOME} element={<Layout />}>
           <Route index element={<Home />} />
-          {/* //TODO: 로그인 컴포넌트 연결해 주세요 */}
           <Route path={LOGIN} element={<Login />} />
           <Route path={SIGN_UP} element={<SignUp />} />
+          {/* 로그인해야지만 갈 수 있는 경로 반영 */}
+          <Route element={<ProtectedRouter />}>
+            <Route path={BOOK_MARK} element={<div> bookmark페이지 pr 확인 후 반영</div>} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
