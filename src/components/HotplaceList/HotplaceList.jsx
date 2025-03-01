31e3fc7d17ef0d84d21dd5fa006d9fe34b89ee8d
JinChaeKim <rlawlsco88@gmail.com>
2025-03-02 03:06:45 +0900

    design : #22 슬라이드 및 스크롤 수정

--- src/components/HotplaceList/HotplaceList.jsx
+++ src/components/HotplaceList/HotplaceList.jsx
@@ -19,30 +19,30 @@ const HotplaceList = () => {
     return <div>에러가 발생했습니다.</div>;
   }
 
+  document.body.style.overflow = isVisible ? 'hidden' : 'auto';
+
   return (
-    <article className='m-3 w-[250px]'>
-      <div
-        className={`${
-          isVisible ? 'slide-up opacity-100' : 'opacity-0'
-        } transition-opacity duration-300 bg-neutral-50 w-[250px] pb-[50px]`}
-      >
-        {hotplaces.map((data) => (
-          <section
-            key={data.id}
-            onClick={() => setSelectedPost(data.id)}
-            className='border-2 w-[250px] p-3 mb-3 bg-neutral-50 cursor-pointer'
-          >
-            <div className='flex gap-4 items-center'>
-              <div className='text-orange-400 text-[23px]'>{data.name}</div>
-              <div className='text-neutral-400 text-[15px]'>{data.category_name}</div>
-            </div>
-            <h3 className='text-[13px]'>{data.address_road_name}</h3>
-            <div className='flex gap-4 items-center'>
-              <div className='text-lime-600'>{data.contact_number}</div>
-            </div>
-          </section>
-        ))}
-      </div>
+    <article className='m-3 w-[250px] overflow-y-auto h-[80vh]'>
+      {isVisible && (
+        <div className='slide-up opacity-100 transition-opacity duration-300 bg-neutral-50 w-[250px] pb-[50px]'>
+          {hotplaces.map((data) => (
+            <section
+              key={data.id}
+              onClick={() => setSelectedPost(data.id)}
+              className='border-2 w-[250px] p-3 mb-3 bg-neutral-50 cursor-pointer'
+            >
+              <div className='flex gap-4 items-center'>
+                <div className='text-orange-400 text-[23px]'>{data.name}</div>
+                <div className='text-neutral-400 text-[15px]'>{data.category_name}</div>
+              </div>
+              <h3 className='text-[13px]'>{data.address_road_name}</h3>
+              <div className='flex gap-4 items-center'>
+                <div className='text-lime-600'>{data.contact_number}</div>
+              </div>
+            </section>
+          ))}
+        </div>
+      )}
       <button onClick={toggleHotPlaceList} className='fixed p-2 w-[250px] bg-button bottom-0 border-2 rounded-t-2xl'>
         {isVisible ? '핫플 닫기' : '핫플 보기'}
       </button>
