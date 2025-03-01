e87812682821159ad93960e5ae7cd9a6edd94232
JinChaeKim <rlawlsco88@gmail.com>
2025-03-01 14:00:00 +0900

    design : #22 디자인 수정

--- src/components/HotplaceList/HotplaceList.jsx
+++ src/components/HotplaceList/HotplaceList.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-import { Spacer } from './Spacer';
 import { useGetHotplaces } from '@/lib/queries/GetHotplaces';
-import { useNavigate } from 'react-router-dom';
 import DetailModal from '../modal/detail-modal';
 
 const HotplaceList = () => {
@@ -22,32 +20,30 @@ const HotplaceList = () => {
   }
 
   return (
-    <aside className='m-3  w-[250px]'>
+    <aside className='m-3 w-[250px]'>
       <div
         className={`${
           isVisible ? 'slide-up opacity-100' : 'opacity-0'
-        } transition-opacity duration-300 bg-neutral-50 w-[250px]`}
+        } transition-opacity duration-300 bg-neutral-50 w-[250px] pb-[50px]`}
       >
-        <div className='overflow-y-auto'>
-          {hotplaces.map((data) => (
-            <section
-              key={data.id}
-              onClick={() => setSelectedPost(data.id)}
-              className='border-2 w-[250px] p-3 mb-3 bg-neutral-50 cursor-pointer'
-            >
-              <div className='flex gap-4 items-center'>
-                <div className='text-orange-400 text-[23px]'>{data.name}</div>
-                <div className='text-neutral-400 text-[15px]'>{data.category_name}</div>
-              </div>
-              <div className='text-[13px]'>{data.address_road_name}</div>
-              <div className='flex gap-4 items-center'>
-                <div className='text-lime-600'>{data.contact_number}</div>
-              </div>
-            </section>
-          ))}
-        </div>
+        {hotplaces.map((data) => (
+          <section
+            key={data.id}
+            onClick={() => setSelectedPost(data.id)}
+            className='border-2 w-[250px] p-3 mb-3 bg-neutral-50 cursor-pointer'
+          >
+            <div className='flex gap-4 items-center'>
+              <div className='text-orange-400 text-[23px]'>{data.name}</div>
+              <div className='text-neutral-400 text-[15px]'>{data.category_name}</div>
+            </div>
+            <div className='text-[13px]'>{data.address_road_name}</div>
+            <div className='flex gap-4 items-center'>
+              <div className='text-lime-600'>{data.contact_number}</div>
+            </div>
+          </section>
+        ))}
       </div>
-      <button onClick={toggleHotPlaceList} className='fixed p-2 w-[250px] bg-button bottom-3 border-2 rounded-t-2xl'>
+      <button onClick={toggleHotPlaceList} className='fixed p-2 w-[250px] bg-button bottom-0 border-2 rounded-t-2xl'>
         {isVisible ? '핫플 닫기' : '핫플 보기'}
       </button>
       {selectedPost && <DetailModal id={selectedPost} />}
--- src/components/HotplaceList/Spacer.jsx
+++
@@ -1,9 +0,0 @@
-export const Spacer = ({ x, y }) => {
-  if (x) {
-    return <div style={{ width: `${x}px` }} />;
-  }
-  if (y) {
-    return <div style={{ height: `${y}px` }} />;
-  }
-  return null;
-};
