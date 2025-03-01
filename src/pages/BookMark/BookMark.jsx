d83d56e230508e3751066962ca81078a15ad8432
JinChaeKim <rlawlsco88@gmail.com>
2025-03-01 20:39:12 +0900

    feat : #36 북마크 페이지 DB 연결

--- src/pages/BookMark/BookMark.jsx
+++ src/pages/BookMark/BookMark.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import DetailModal from '@/components/modal/detail-modal';
 import { useGetBookmarks } from '@/lib/queries/GetBookmarks';
 import { useState } from 'react';
@@ -5,7 +6,8 @@ import { useState } from 'react';
 const BookMark = () => {
   const { data: bookmarkList = [], isPending, isError } = useGetBookmarks();
   const booksNum = bookmarkList.length;
-  const [selectPlace, setSelectPlace] = useState(null);
+  const [selectPost, setSelectPost] = useState(null);
+  const userNickName = localStorage.getItem('nickname');
 
   if (isPending) {
     return <div>로딩 중...</div>;
@@ -21,7 +23,7 @@ const BookMark = () => {
       <section className='flexCenter pt-[70px]'>
         <div className='flex flex-col md:flex-row gap-[120px] justify-evenly items-center rounded-lg'>
           <div className='flex flex-wrap text-[26px]'>
-            <span className='text-accent font-bold'>{/* {bookmarkList[0].users.nickname} */}</span>님 안녕하세요!
+            <span className='text-accent font-bold'>{userNickName}</span>님 안녕하세요!
           </div>
           <div className='flexCenter flex-col gap-6 bg-neutral-200 w-[170px] h-[170px] rounded-full'>
             <p className='text-[20px] font-bold'>북마크 수</p>
@@ -30,17 +32,17 @@ const BookMark = () => {
         </div>
       </section>
       {booksNum === 0 && <p className='flexCenter mt-[90px]'>아직 북마크 된 장소가 없습니다!</p>}
-      <main className='mx-3 mt-[120px] grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
+      <main className='grid place-items-center mt-[120px] grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
         {bookmarkList.map((data) => (
-          <div key={data.id} onClick={() => setSelectPlace(data.id)} className='flexCenter mb-[20px]'>
-            <section className='relative m-3 bg-neutral-50 rounded-lg pb-3 cursor-pointer shadow-lg hover:-translate-y-3 transition-transform duration-300 ease-in-out'>
+          <React.Fragment key={data.id} onClick={() => setSelectPost(data.id)} className='flexCenter mb-[20px]'>
+            <section className='relative m-3 w-[280px] bg-neutral-50 rounded-lg pb-3 cursor-pointer shadow-lg hover:-translate-y-3 transition-transform duration-300 ease-in-out'>
               <img src={data.hotplaces.img_url} className='bg-neutral-400 h-[300px] w-[280px] rounded-t-lg' />
               <div className='flexCenter'>
                 <p className='flexCenter mt-3 text-[20px]'>{data.hotplaces.name}</p>
               </div>
             </section>
-            {selectPlace && <DetailModal id={selectPlace} />}
-          </div>
+            {selectPost && <DetailModal id={selectPost} />}
+          </React.Fragment>
         ))}
       </main>
     </div>
