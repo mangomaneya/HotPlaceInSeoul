2006cfb6dcbeccdb3619f5fed610d5c992403798
JinChaeKim <rlawlsco88@gmail.com>
2025-03-04 22:02:14 +0900

    refactor : #58 useAreaStore 연결 준비

--- src/lib/queries/GetHotplaces.js
+++ src/lib/queries/GetHotplaces.js
@@ -2,15 +2,23 @@ import { QUERY_KEYS } from '@/constants/query-keys';
 import supabase from '@/lib/api/supabaseAPI';
 import { useQuery } from '@tanstack/react-query';
 
-export const useGetHotplaces = () => {
+export const useGetHotplaces = ({ area = null }) => {
   const { HOTPLACE } = QUERY_KEYS;
+  const { selectedArea } = useAreaStore((area) => area.selectedArea);
   const getHotplaces = async () => {
-    const { data, error } = await supabase.from('hotplaces').select('*');
+    const { data, error } = await supabase.from(HOTPLACE).select('*').eq('area', selectedArea);
     if (error) {
       throw new Error(error.message);
     }
     return data;
   };
 
-  return useQuery({ queryKey: [HOTPLACE], queryFn: getHotplaces, staleTime: 60 * 60 * 1000 }); //1시간
+  const filterArea = (places) => {
+    if (area) {
+      return places.filter((place) => place.area === selectedArea.area);
+    }
+    return places;
+  };
+
+  return useQuery({ queryKey: [HOTPLACE], queryFn: getHotplaces, select: filterArea, staleTime: 60 * 60 * 1000 }); //1시간
 };
