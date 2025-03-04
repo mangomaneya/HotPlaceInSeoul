2587be677131501d59f4ba69958f3892e71f8c37
bluepome98 <wpdn1998@gmail.com>
2025-03-04 14:35:18 +0900

    #15 에러 수정

--- src/components/map-api/KakaoMap.jsx
+++ src/components/map-api/KakaoMap.jsx
@@ -42,7 +42,7 @@ function KakaoMap() {
       const map = new kakao.maps.Map(mapContainer.current, options);
       //지도생성
       if (markerData.length === 0) {
-        openAlert({ ERROR, text: '마커 데이터를 불러오는데 실패했습니다, 새로고침해주세요' });
+        openAlert({ type:ERROR, text: '마커 데이터를 불러오는데 실패했습니다, 새로고침해주세요' });
         return;
       }
       markerData.forEach((item) => {
