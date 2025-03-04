69d1cd205d6baebe0f0d635e475db060b77f67e2
choichangyeon <changyon99@naver.com>
2025-03-04 17:27:54 +0900

    feat:#57 NotFounded 페이지 생성

--- src/pages/NotFoundedPage.jsx
+++ src/pages/NotFoundedPage.jsx
@@ -1,5 +1,24 @@
+import { PATH } from '@/constants/path-constant';
+import { useNavigate } from 'react-router-dom';
+
 function NotFoundedPage() {
-  return <div>NotFoundedPage</div>;
+  const navigate = useNavigate();
+  const { HOME } = PATH;
+
+  return (
+    <div className='flex-col flexCenter'>
+      <header>
+        <h1 className='font-gugi text-[50px]'>NOT FOUNDED PAGES</h1>
+      </header>
+      <main className='mt-[100px]'>
+        <section>
+          <button onClick={() => navigate(HOME)} className='text-[20px]'>
+            홈으로 돌아가기
+          </button>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default NotFoundedPage;
