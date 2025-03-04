ab3436b2d2aa983943385853a662fa3e0e570884
bluepome98 <wpdn1998@gmail.com>
2025-03-04 21:39:18 +0900

    #19 문법 수정

--- src/constants/store-constant.js
+++ src/constants/store-constant.js
@@ -1,4 +1,4 @@
-export const STORE_CONSTANT = {
+export const STORE_CONSTANT = Object.freeze({
   STORE_NAME: 'name',
   STORE_ADDRESS: 'address_road_name',
   STORE_CONTACT: 'contact_number',
@@ -6,4 +6,4 @@ export const STORE_CONSTANT = {
   STORE_PIC: 'img_url',
   BUSINESS_HOUR: 'open_hours',
   AREA: 'area',
-}; //@TODO: 현재 기준 supabase의 data table을 기준으로 가져온 key입니다.
+}); //@TODO: 현재 기준 supabase의 data table을 기준으로 가져온 key입니다.
