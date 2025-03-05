48af901e83068ee61616c2bb6195fb14ab0ca94a
JinChaeKim <rlawlsco88@gmail.com>
2025-03-05 10:28:58 +0900

    refactor : #58 커스텀 훅 수정

--- src/lib/queries/GetHotplaces.js
+++ src/lib/queries/GetHotplaces.js
@@ -1,11 +1,9 @@
 import { QUERY_KEYS } from '@/constants/query-keys';
 import supabase from '@/lib/api/supabaseAPI';
-import useAreaStore from '@/store/zustand/useAreaStore';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetHotplaces = ({ area = null } = {}) => {
   const { HOTPLACE } = QUERY_KEYS;
-  const selectedArea = useAreaStore((state) => state.selectedArea);
   const getHotplaces = async () => {
     const { data, error } = await supabase.from(HOTPLACE).select('*').eq('area', area); // area:현재 지역
     if (error) {
@@ -15,11 +13,11 @@ export const useGetHotplaces = ({ area = null } = {}) => {
   };
 
   const filterArea = (places) => {
-    return places.filter((place) => place.area === selectedArea);
+    return places.filter((place) => place.area === area);
   };
 
   return useQuery({
-    queryKey: [HOTPLACE, selectedArea || 'default'], //selectedArea가 null 일 때는 default로 캐싱
+    queryKey: area ? [HOTPLACE, area] : [HOTPLACE], // area가 없는 전체값은 HOTPLACE로 캐싱
     queryFn: getHotplaces,
     select: filterArea, // 가공한 값을 전달
     staleTime: 60 * 60 * 1000, //1시간
