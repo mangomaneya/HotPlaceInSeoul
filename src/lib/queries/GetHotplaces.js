6edfb767cdeb07885e53c8033fa58210ec7e8cf4
JinChaeKim <rlawlsco88@gmail.com>
2025-03-05 10:53:09 +0900

    refactor : #58 selectedArea 추가 로직으로 수정

--- src/lib/queries/GetHotplaces.js
+++ src/lib/queries/GetHotplaces.js
@@ -1,9 +1,10 @@
 import { QUERY_KEYS } from '@/constants/query-keys';
 import supabase from '@/lib/api/supabaseAPI';
+import useAreaStore from '@/store/zustand/useAreaStore';
 import { useQuery } from '@tanstack/react-query';
-
 export const useGetHotplaces = ({ area = null } = {}) => {
   const { HOTPLACE } = QUERY_KEYS;
+  const selectedArea = useAreaStore((state) => state.selectedArea);
   const getHotplaces = async () => {
     const { data, error } = await supabase.from(HOTPLACE).select('*').eq('area', area); // area:현재 지역
     if (error) {
@@ -11,9 +12,8 @@ export const useGetHotplaces = ({ area = null } = {}) => {
     }
     return data; // 현재 지역에 해당하는 area를 가진 테이블의 값들을 return
   };
-
   const filterArea = (places) => {
-    return places.filter((place) => place.area === area);
+    return places.filter((place) => place.area === selectedArea);
   };
 
   return useQuery({
@@ -21,6 +21,6 @@ export const useGetHotplaces = ({ area = null } = {}) => {
     queryFn: getHotplaces,
     select: filterArea, // 가공한 값을 전달
     staleTime: 60 * 60 * 1000, //1시간
-    enabled: !!area, // area가 null이 아닌 값이 경우 실행
+    enabled: !!area, // selectedArea가 존재할 때만 실행
   });
 };
